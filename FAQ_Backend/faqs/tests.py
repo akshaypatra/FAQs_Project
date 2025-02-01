a4ff4d0a88574884ee72c8288f2cb6f5005c46fc
Akshay Patra <akshay.patra114@gmail.com>
2025-02-01 12:09:10 +0530

    feat : added tests and ran the test succesfully

--- FAQ_Backend/faqs/tests.py
+++ FAQ_Backend/faqs/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import FAQ
 
-# Create your tests here.
+class FAQModelTest(TestCase):
+    def setUp(self):
+        self.faq = FAQ.objects.create(question="Hello", answer="This is a test FAQ", language='en')
+
+    def test_translation(self):
+        self.assertIsNotNone(self.faq.get_translated_question('hi'))
