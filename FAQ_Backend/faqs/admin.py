eaa9a60712dd500645c03cbbedde57b191f33cf0
Akshay Patra <akshay.patra114@gmail.com>
2025-02-01 11:45:55 +0530

    feat : registering the models in admin site

--- FAQ_Backend/faqs/admin.py
+++ FAQ_Backend/faqs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import FAQ, FAQTranslation
 
-# Register your models here.
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'language')
+
+class FAQTranslationAdmin(admin.ModelAdmin):
+    list_display = ('faq', 'language', 'translated_question')
+
+admin.site.register(FAQ, FAQAdmin)
+admin.site.register(FAQTranslation, FAQTranslationAdmin)
+
